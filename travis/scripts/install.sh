ce35ab0430737277073695b8aee6be1bce1eca1d
Yeoh Shin Shen <shinshen.yeoh@jobstreet.com>
2016-08-02 18:12:10 +0800

    Gradle settings

--- travis/scripts/install.sh
+++ travis/scripts/install.sh
@@ -27,10 +27,10 @@ elif [ "$BUILD_TYPE" = jobst-and  ]
 then
   echo ">> Decrypt Keystore"
   #openssl aes-256-cbc -k ${ENCRYPT_PASS} -in travis/scripts/cert/travisPOC.keystore.enc -out travis/scripts/cert/travisPOC.keystore -d -a
-  mv travis/scripts/cert/f8app.jks android/app/f8app.jks
+  #mv travis/scripts/cert/f8app.jks android/app/f8app.jks
   echo ">> Set Gradle Properties"
-  echo "RELEASE_STORE_FILE=f8app.jks" >> ~/.gradle/gradle.properties
-  echo "RELEASE_KEY_ALIAS=travis-poc" >> ~/.gradle/gradle.properties
-  echo "RELEASE_STORE_PASSWORD=seekasia" >> ~/.gradle/gradle.properties
-  echo "RELEASE_KEY_PASSWORD=seekasia" >> ~/.gradle/gradle.properties
+  echo "RELEASE_STORE_FILE=f8app.jks" >> android/gradle.properties
+  echo "RELEASE_KEY_ALIAS=travis-poc" >> android/gradle.properties
+  echo "RELEASE_STORE_PASSWORD=seekasia" >> android/gradle.properties
+  echo "RELEASE_KEY_PASSWORD=seekasia" >> android/gradle.properties
 fi
